9c98f20200f8174dcc72d79d4fd4be181941d2c4
Justin9192 <61888788+Justin9192@users.noreply.github.com>
2023-01-12 15:00:50 -0800

    1 file modified

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function calcBet(a,b) {
 }
 
 // code below = how much you win 
-myCheckbox.addEventListener("click", calcBet);
+calculator.addEventListener("click", calcBet);
 
 
 // need function to take form inputs and perform math
