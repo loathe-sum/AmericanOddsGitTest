f6401e9d841488ad14e09b719426d1573eef1158
Justin9192 <61888788+Justin9192@users.noreply.github.com>
2023-01-12 10:28:04 -0800

    3 files modified

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ function calcBet(a,b) {
   var a = parseInt(document.getElementById('betAmt').value);
   var b = parseInt(document.getElementById('betOdds').value);
   if (b > 0) {
-    let poop = b / 100;
-    let poop0 = poop * a;
-    let poopAlert = parseFloat(poop0).toFixed(2);
-    document.getElementById('chokeMe').innerHTML = ('To Win: $' + poopAlert);
+    let c = b / 100;
+    let plusOdds = c * a;
+    let plusOddsFinal = parseFloat(plusOdds).toFixed(2);
+    document.getElementById('chokeMe').innerHTML = ('To Win: $' + plusOddsFinal);
   } 
   else if (b < 0) {
-    let pee = b * -1;
-    let urine = 100 / pee;
-    let urine0 = urine * a;
-    let peeAlert = parseFloat(urine0).toFixed(2);
-    document.getElementById('chokeMe').innerHTML = ('To Win: $' + peeAlert);
+    let c = b * -1;
+    let minusOddsFlip = 100 / c;
+    let minusOddsInt = minusOddsFlip * a;
+    let minusOddsFinal = parseFloat(minusOddsInt).toFixed(2);
+    document.getElementById('chokeMe').innerHTML = ('To Win: $' + minusOddsFinal);
   }
   else {
     document.getElementById('chokeMe').innerHTML = ('Plug in numbers dumbass');
@@ -20,7 +20,7 @@ function calcBet(a,b) {
 }
 
 // code below = how much you win 
-fuckMe.addEventListener("click", calcBet);
+calculate.addEventListener("click", calcBet);
 
 
 // need function to take form inputs and perform math
